33da4a4ee5239b8c85dd9321ec50a16e4856900f
UglukFearless <UglukFearless@mail.ru>
2026-03-25 01:10:50 +0700

    Add filter component, fix some styles

---
+++ frontend/src/components/Statistic/StatisticFilter.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="statistic-filter">
+        <span class="statistic-filter__title">{{ filter.title }}</span>
+        <select class="statistic-filter__select" v-model="filter.value" @change="updateValue(filter.value)">
+            <option 
+                class="statistic-filter__option"
+                v-for="option in filter.options" 
+                :value="option.value">
+                {{ option.label }}
+            </option>
+        </select>
+    </div>
+</template>
+
+<script lang="ts" setup>
+import { PropType } from 'vue';
+
+const props = defineProps({
+    filter: {
+        required: true,
+        type: Object as PropType<any>,
+    }
+});
+
+const emit = defineEmits(['update:modelValue']);
+
+function updateValue(filterValue: string | number) {
+    if (typeof filterValue === 'string') {
+        filterValue = Number.parseInt(filterValue);
+    }
+    emit('update:modelValue', filterValue);
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
--- frontend/src/components/Statistic/StatisticFiltersBlock.vue
+++ frontend/src/components/Statistic/StatisticFiltersBlock.vue
@@ -1,3 +1,37 @@
 <template>
-    Фильтры
+    <div class="statistic-filters-block">
+        <div class="statistic-filters-block__filter-item">
+            <span class="statistic-filters-block__filter-title">Параметр:</span>
+            <select>
+                <option value="0">Количество побед</option>
+                <option value="1">Богатство</option>
+                <option value="2">Опыт</option>
+            </select>
+        </div>
+        <div class="statistic-filters-block__filter-item">
+            <span class="statistic-filters-block__filter-title">Жив/мёртв:</span>
+            <select>
+                <option value="0">Все</option>
+                <option value="1">Живые</option>
+                <option value="2">Мёртвые</option>
+            </select>
+        </div>
+        <div class="statistic-filters-block__filter-item">
+            <span class="statistic-filters-block__filter-title">Хазяин:</span>
+            <select>
+                <option value="0">Все</option>
+                <option value="1">Мои</option>
+            </select>
+        </div>
+        <div class="statistic-filters-block__filter-item">
+            <span class="statistic-filters-block__filter-title">Порядок:</span>
+            <select>
+                <option value="0">По возрастанию</option>
+                <option value="1">По убыванию</option>
+            </select>
+        </div>
+    </div>
 </template>
+
+<script lang="ts" setup>
+</script>
